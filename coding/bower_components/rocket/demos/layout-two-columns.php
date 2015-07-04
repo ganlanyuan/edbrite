7cf82eb474de2819829d94abc4a0e4d9ac8a5a67
ganlanyuan <linwilliam@Williams-iMac.home>
2015-07-04 14:10:17 -0400

    add coding folder

---
+++ coding/bower_components/rocket/demos/layout-two-columns.php
@@ -0,0 +1,36 @@
+<?php include 'include/head.php'; ?>
+<body>
+<div class="page">
+  <div class="container">
+
+    <div class="topic">
+      <h2 id=""><span>layout: </span>two columns</h2>
+      <div class="example example-two-columns" data-margin>
+        <div data-col-main>
+          <div class="example-content" data-content>
+            main
+          </div>
+        </div>
+        <div data-col-aside>
+          <div class="example-content" data-content>
+            aside: 200px
+          </div>
+        </div>
+      </div>
+      <pre><code class="language-scss">
+.example-two-columns { 
+  @include two-columns(right 200px gutter 20px); 
+  @include breakpoint('min' 800) {
+    @include two-columns(left 300px gutter 20px);
+  }
+}
+      </code></pre>
+    </div>
+    
+    <?php include "include/more-demos.php" ?>
+  </div>
+  <?php include "include/site-footer.php" ?>
+</div>
+</body>
+</html>
+
---
+++ coding/bower_components/rocket/src/js/kit.js
@@ -0,0 +1,9 @@
+// *** base ***
+// @codekit-prepend "kit/core.js"
+// @codekit-prepend "kit/reach.js"
+// @codekit-prepend "kit/scrollTo.js"
+// @codekit-prepend "kit/numIncrease.js"
+// @codekit-prepend "kit/animate.js"
+
+// *** components ***
+// @codekit-prepend "components/offcanvas.js"
